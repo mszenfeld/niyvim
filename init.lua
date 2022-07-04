300778efad8d8564b96a479bbf0a1ec132df7e1c
Marian Szenfeld <mef1st0@protonmail.com>
2022-07-04 15:44:46 +0200

    feat(Config): Add support for custom configuration

--- init.lua
+++ init.lua
@@ -14,17 +14,12 @@ vim.g.fold_method = 1
 
 -- Tests
 vim.g['test#strategy'] = 'dispatch'
-vim.g['test#python#runner'] = 'pytest'
 
 -- Emmet
 vim.g.user_emmet_leader_key = ','
 
--- Go
-vim.g.go_def_mapping_enabled = 0
+local config_file = io.open('config.lua', r)
 
--- PEP8
-vim.g.python_pep8_indent_multiline_string = -1
-vim.g.python_pep8_indent_hang_closing = 0
-
--- Svelte
-vim.g.svelte_preprocessors = {'typescript'}
+if config_file ~= nil and io.close(config_file) then
+  require('config')
+end
